270ca94ef003100dd0b495bb0f23c6559c625e63
Joakim Wassberg <joakim@arthead.se>
2022-04-20 19:44:52 +0200

    Added test

---
+++ test/test_first.py
@@ -0,0 +1,19 @@
+import pytest
+from app import create_app
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+
+    app.config['TESTING'] = True
+
+    with app.app_context():
+        with app.test_client() as client:
+            
+            yield client
+
+def test_first(client):
+    response = client.get('/api/v1.0/first')
+    assert response.status_code == 200
+    
